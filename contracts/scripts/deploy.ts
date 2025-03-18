8e3d07bc57266b846414f6fcab3786db3194818a
HoangDucBach <hoangbach0985@gmail.com>
2025-03-18 23:30:33 +0700

    Deploy successfull

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,8 +1,7 @@
 import hre from "hardhat";
 
-var NB_STAKING_REWARD_ADDRESS = "0x0000000000000000000000000000000000000001";
-var NB_UTILITY_NFT_ADDRESS = "";
-var API3_READER_PROXY_ADDRESS = "";
+var NB_STAKING_REWARD_ADDRESS = "0x0CfAe8A1f3D56439bBa4733BB16D8A96b4eA74E6";
+var NB_UTILITY_NFT_ADDRESS = "0xa34d09E2Cfc3F22CB77a9e4E81593Bb2b85c3002";
 
 async function deploy() {
     const NBUtilityNFT = await hre.ethers.getContractFactory("NBUtilityNFT");
@@ -12,7 +11,7 @@ async function deploy() {
     console.log("NBUtilityNFT deployed to:", NB_UTILITY_NFT_ADDRESS);
 
     const NBStakingReward = await hre.ethers.getContractFactory("NBStakingReward");
-    const nbStakingReward = await NBStakingReward.deploy();
+    const nbStakingReward = await NBStakingReward.deploy(NB_STAKING_REWARD_ADDRESS, NB_UTILITY_NFT_ADDRESS);
     await nbStakingReward.waitForDeployment();
     NB_STAKING_REWARD_ADDRESS = await nbStakingReward.getAddress();
     console.log("NBStakingReward deployed to:", NB_STAKING_REWARD_ADDRESS);
